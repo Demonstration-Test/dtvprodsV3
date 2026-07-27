c74f6f82310a718a86852713017f434813d73054
RubyDaG <anotherstory713@gmail.com>
2026-07-27 04:18:24 -0400

    fix Pages dependency install

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: "/dtvprodsV3/",
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   build: {
     target: "es2022",
     cssCodeSplit: true,
